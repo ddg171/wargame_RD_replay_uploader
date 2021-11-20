cfdfbcbc6d242568022b5391a2adf1fffe375a75
TY <ddsamidare@gmail.com>
2021-11-20 14:57:44 +0900

    devserverを設定

--- wr_frontend/vue.config.js
+++ wr_frontend/vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  port:8080,
   transpileDependencies: ["vuetify"],
-  outputDir:"../wr_server_front/src/dist"
+  outputDir:"../wr_server_front/src/dist",
+  devServer:{
+    proxy: {
+      '/api/': {
+          target: 'http://localhost:3000'
+      }
+  }
+  }
 };
